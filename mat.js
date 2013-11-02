ed86f5cc3417827200e83de85c7c4df395327612
Luke Blaney <git@lukeblaney.co.uk>
2013-11-02 16:42:25 +0000

    Render the empty mat onto the canvas

--- mat.js
+++ mat.js
@@ -5,6 +5,20 @@
  * @returns {Routine}
  */
 function Mat(canvas) {
+	var height, width, context = canvas.getContext("2d");
+
+	// Number of pixels which are equivalent to 6 foot in real life;
+	var scale = 100;
+
+	// The colour of the mat
+	var matColor = "rgb(0,0,100)";
+
+	// The colour of the guide lines placed onto the mat
+	var lineColor = "rgb(100, 100, 100)";
+	height = 7 * scale;
+	width = 9 * scale;
+
+
 	if (canvas.tagName.toLowerCase() != "canvas") {
 		throw "Canvas element must be passed into init";
 	}
@@ -14,11 +28,43 @@ function Mat(canvas) {
 	canvas.setAttribute("data-cheer-routine-inited", true);
 
 	/**
+	 * Renders an empty mat with standard guide lines on it
+	 */
+	function renderMat() {
+
+		canvas.height = height;
+		canvas.width = width;
+
+		// Draw the blue background of the matts
+		context.fillStyle = matColor;
+		context.fillRect(0, 0, width, height);
+
+		// Draws the lines between strips
+		context.fillStyle = lineColor;
+		for (i=0; i<=9; i++) {
+			context.fillRect(i*scale - 2, 0, 4, height);
+		}
+
+		// Draw the X in the centre
+		var centresize = 10;
+		context.strokeStyle = lineColor;
+		context.lineWidth = 3;
+		context.beginPath();
+		context.moveTo(width/2 - centresize, height/2 - centresize);
+		context.lineTo(width/2 + centresize, height/2 + centresize);
+		context.moveTo(width/2 - centresize, height/2 + centresize);
+		context.lineTo(width/2 + centresize, height/2 - centresize);
+		context.stroke();
+	}
+
+
+	/**
 	 * Renders a list of action objects onto the mat
-	 * @param {object} actions A an object of key value pairs, where key is the person id and value is an Action object
+	 * @param {object} actions An object of key value pairs, where key is the person id and value is an Action object
 	 */
 	function renderActions (actions) {
-		var personid;
+		var i, personid;
+		renderMat();
 		for (personid in actions) {
 
 			// TODO: actually render the points on the mat
